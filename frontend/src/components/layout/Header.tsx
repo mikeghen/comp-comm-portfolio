c2f588f3d6326635acf9f12cab2df638840b18ca
Michael Ghen <mike@mikeghen.com>
2025-09-28 11:09:56 -0400

    Add about link to Compound Forum

--- frontend/src/components/layout/Header.tsx
+++ frontend/src/components/layout/Header.tsx
@@ -12,9 +12,9 @@ function Header() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav" className="justify-content-between">
           <Nav className="me-auto">
-            {/* <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#docs">Documentation</Nav.Link>
-            <Nav.Link href="#about">About</Nav.Link> */}
+            <Nav.Link href="https://www.comp.xyz/t/compound-community-portfolio-pay-to-govern-p2g-experiment/7228" target="_blank" rel="noopener noreferrer">
+              About
+            </Nav.Link>
           </Nav>
           <div className="wallet-connect-container">
             <ConnectButton />
